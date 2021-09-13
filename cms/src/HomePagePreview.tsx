c5d2267ce49590a6256728f1d9a449617af48036
fraol0912 <fraol0912@gmail.com>
2021-09-13 17:00:59 +0300

    add home page preview

--- cms/src/HomePagePreview.tsx
+++ cms/src/HomePagePreview.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { HomePageTemplate } from "../../template/HomePage";
 
 export default function HomePagePreview({ entry }: { entry: any }) {
-  console.log(entry.getIn(["data", "headline_title"]));
+  const headlineTitle = entry.getIn(["data", "headline_title"]);
+  const headlineDetail = entry.getIn(["data", "headline_detail"]);
+  const headlineImage = entry.getIn(["data", "headline_image"]);
 
-  return <h1>Hello</h1>;
+  const headlineButton1Label = entry.getIn(["data", "headline_button1_label"]);
+  const headlineButton1Link = entry.getIn(["data", "headline_button1_link"]);
+
+  const headlineButton2Label = entry.getIn(["data", "headline_button2_label"]);
+  const headlineButton2Link = entry.getIn(["data", "headline_button2_link"]);
+
+  return (
+    <HomePageTemplate
+      indexPageData={{
+        headlineTitle,
+        headlineDetail,
+        headlineImage,
+        headlineButton1Label,
+        headlineButton1Link,
+        headlineButton2Label,
+        headlineButton2Link,
+      }}
+      services={[]}
+    />
+  );
 }
