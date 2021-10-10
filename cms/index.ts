cedc93475d13aa0bdfa67aaab0efa966a8aec300
fraol0912 <fraol0912@gmail.com>
2021-10-10 18:52:52 +0300

    change name of cms index file

---
+++ cms/index.ts
@@ -0,0 +1,27 @@
+import CMS from "netlify-cms-app";
+
+// Collections
+import { pages } from "./collections/pages";
+import { tests } from "./collections/tests";
+import { blogs } from "./collections/blogs";
+import { services } from "./collections/services";
+import { testimonials } from "./collections/testimonials";
+
+CMS.init({
+  config: {
+    backend: {
+      name: "git-gateway",
+      branch: "main",
+    },
+    editor: {
+      preview: false,
+    },
+    local_backend: true,
+    public_folder: "/image",
+    media_folder: "public/image",
+    site_url: "https://mrc-et.netlify.app",
+    display_url: "https://mrc-et.netlify.app",
+    logo_url: "/logo.webp",
+    collections: [pages, tests, services, testimonials, blogs],
+  },
+});
