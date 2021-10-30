ff6d0500446ba30f8feef90d493ffebafa3383dd
fraol0912 <fraol0912@gmail.com>
2021-10-30 07:52:05 +0300

    404 page

---
+++ pages/404.tsx
@@ -0,0 +1,26 @@
+// NextJs
+import NextLink from "next/link";
+
+// Chakra UI
+import { Divider, Heading } from "@chakra-ui/layout";
+
+// Components
+import Seo from "../components/utils/Seo";
+import PrimaryButton from "../components/utils/PrimaryButton";
+import MainContainer from "../components/utils/MainContainer";
+
+export default function NotFoundPage() {
+  return (
+    <>
+      <Seo title="Not Found" />
+      <MainContainer>
+        <Heading my={5}>The page you requested was not found.</Heading>
+        <NextLink href="/">
+          <a>
+            <PrimaryButton my={5}>Go to Home Page</PrimaryButton>
+          </a>
+        </NextLink>
+      </MainContainer>
+    </>
+  );
+}
