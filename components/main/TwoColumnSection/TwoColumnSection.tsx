69c01e3b21edb957b441dfcf457d101c8ae0c5d2
fraol0912 <fraol0912@gmail.com>
2022-01-12 21:19:51 +0300

    increase font size on blog cards

--- components/main/TwoColumnSection/TwoColumnSection.tsx
+++ components/main/TwoColumnSection/TwoColumnSection.tsx
@@ -36,7 +36,9 @@ export default function TwoColumnSection({
       </MotionBox>
       <VStack align="flex-start" justify="space-between">
         <Heading size="lg">{title}</Heading>
-        <Text my={2}>{text}</Text>
+        <Text my={2} fontSize="lg">
+          {text}
+        </Text>
         <NextLink href={link}>
           <a>
             <PrimaryButton title={`Go to ${title} page`}>
